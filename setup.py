35bee38186c7265385a70edc5115cd9768ae8c1b
Pratik K <pratikk@proximabiz.com>
2021-06-04 19:11:12 +0530

    minor fixes

--- setup.py
+++ setup.py
@@ -1,26 +1,82 @@
-import setuptools
+from distutils.core import setup
+from setuptools import find_packages
+import os
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="spdx-venv-demo-ifpratik",
-    version="0.0.1",
-    author="Pratik Karanje",
-    author_email="lfpratik@linuxfoundation.org",
-    description="A small example package",
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+setup(
+    # Name of the package
+    name="spdx-pipenv-demo",
+
+    # Packages to include into the distribution
+    packages=find_packages('.'),
+
+    # Start with a small number and increase it with every change you make
+    # https://semver.org
+    version='0.0.1',
+
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    # For example: MIT
+    license='apache-2.0',
+
+    # Short description of your library
+    description='A small example package',
+
+    # Long description of your library
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/lfpratik/spdx-venv-demo",
+    long_description_context_type='text/markdown',
+
+    # Your name
+    author='Pratik Karanje',
+
+    # Your email
+    author_email='lfpratik@linuxfoundation.org',
+
+    # Either the link to your github or to your website
+    url='https://github.com/lfpratik/spdx-pipenv-demo',
+
+    # Project Urls
     project_urls={
-        "Bug Tracker": "https://github.com/lfpratik/spdx-venv-demo/issues",
+        "Bug Tracker": "https://github.com/lfpratik/spdx-pipenv-demo/issues",
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+
+    # Link from which the project can be downloaded
+    download_url='',
+
+    # List of keyword arguments
+    keywords=[],
+
+    # List of packages to install with this one
+    install_requires=[
+        "iso8601",
+        "click",
+        "markdown",
     ],
-    package_dir={"": "spdx_venv_demo"},
-    packages=setuptools.find_packages(where="spdx_venv_demo"),
+
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache license 2.0",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+
+    # Required Python version
     python_requires=">=3.6",
 )
